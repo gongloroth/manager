6cf674b1d7dd1dca0c2ff26e8490b43f0bc9a3f5
gongloroth <gnambence@gmail.com>
2018-02-16 13:08:06 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { creatStore } from 'redux';
+
+class App extends Component {
+  render() {
+    return() {
+      <Provider store={creatStore()}>
+        <View>
+          <Text>
+            Hello!
+          </Text>
+        </View>
+      </Provider>
+    }
+  }
+}
+
+export default App;
